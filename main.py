027464b682f06669b71774a4828d4eed357e4610
EdoCapo <e.caporin@gmail.com>
2025-03-03 17:47:11 +0100

    label ordering

---
+++ main.py
@@ -0,0 +1,15 @@
+from src.utils.labels_ordering import train_labels, val_labels, test_labels
+
+
+def main():
+    train_path = 'data/Train/train_labels.csv'
+    test_path = 'data/Test/test_labels.csv'
+    val_path = 'data/Val/val_labels.csv'
+
+    train_labels(train_path)
+    test_labels(test_path)
+    val_labels(val_path)
+
+
+if __name__ == '__main__':
+    main()
