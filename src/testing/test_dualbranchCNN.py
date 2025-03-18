3c24fcee8647047a1c5411b917733a7afda77f17
EdoCapo <e.caporin@gmail.com>
2025-03-18 14:42:46 +0100

    error fixing

--- src/testing/test_dualbranchCNN.py
+++ src/testing/test_dualbranchCNN.py
@@ -1,65 +1,73 @@
 import os
 import tensorflow as tf
-import numpy as np
 from tensorflow.keras import losses
 
 from src.Utils.filelabels_search import filelabels_search
+from src.Utils.load_tensors import load_tensor
 
 
 def test_model(model, test_files_path='data/Test/tensors', batch_size=32):
     """
-    Funzione per testare il modello sui dati di test.
+    Funzione per testare il modello e calcolare l'errore percentuale medio, la loss e l'errore medio in mesi sui dati di test.
     """
+
     # Carica i dati di test
-    test_files = [f for f in os.listdir(test_files_path) if f.endswith('.pt')]
+    test_files = [f for f in os.listdir(test_files_path) if f.endswith('.data-00000-of-00001')]
     test_data = {"pooled": [], "heatmap": [], "gender": [], "age": []}
-    labels_path = os.path.join(test_files_path, "..", "train_labels.csv")
-    heatmaps_path = os.path.join(test_files_path, "..", "heatmaps")
+    test_labels_path = os.path.join(test_files_path, "..", "test_labels.csv")
     for file in test_files:
-        file_path = os.path.join(test_files_path, file)
+        file_prefix = os.path.join(test_files_path, file.split(".data")[0])
         try:
-            id_img = int(os.path.splitext(file)[0])
+            id_img = int(file.split("_")[1].split(".data")[0])
         except ValueError:
             print(f"Nome file non valido, saltato: {file}")
             continue
-        pooled_tensor = tf.convert_to_tensor(np.load(file_path), dtype=tf.float32)
-        heatmap_file = f"{id_img}.pt"
-        heatmap_tensor_path = os.path.join(heatmaps_path, heatmap_file)
-        if not os.path.exists(heatmap_tensor_path):
-            print(f"Heatmap non trovata per l'immagine {id_img}, saltata.")
-            continue
-        heatmap_tensor = tf.convert_to_tensor(np.load(heatmap_tensor_path), dtype=tf.float32)
-        gender, age = filelabels_search(labels_path, id_img)
-        test_data["pooled"].append(pooled_tensor)
-        test_data["heatmap"].append(heatmap_tensor)
+        pooled_tensor = load_tensor(file_prefix.replace("heatmaps", "tensors"), "tensor/.ATTRIBUTES/VARIABLE_VALUE")
+        heatmap_tensor = load_tensor(file_prefix, "heated/.ATTRIBUTES/VARIABLE_VALUE")
+
+        gender, age = filelabels_search(test_labels_path, id_img)
+        test_data["pooled"].append(tf.transpose(pooled_tensor, perm=[1, 2, 0]))
+        test_data["heatmap"].append(tf.transpose(heatmap_tensor, perm=[1, 2, 0]))
         test_data["gender"].append(gender)
         test_data["age"].append(age)
+
     gender_mapping = {"M": 1, "F": 0}
     test_data["gender"] = [gender_mapping[g] if isinstance(g, str) else g for g in test_data["gender"]]
     test_pooled_tensors = tf.stack(test_data["pooled"])
     test_heatmap_tensors = tf.stack(test_data["heatmap"])
     test_gender_tensors = tf.convert_to_tensor(test_data["gender"], dtype=tf.float32)
-    test_age_tensors = tf.convert_to_tensor(test_data["age"], dtype=tf.float32)
+
+    test_age_corrected = [float(s.replace(',', '.')) for s in test_data["age"]]
+    test_age_tensors = tf.convert_to_tensor(test_age_corrected, dtype=tf.float32)
+
     test_dataset = tf.data.Dataset.from_tensor_slices(
         (test_pooled_tensors, test_heatmap_tensors, test_gender_tensors, test_age_tensors))
-    test_dataset = test_dataset.batch(batch_size)
+    test_dataset = test_dataset.batch(batch_size, drop_remainder=True)
 
-    # Valutazione del modello sui dati di test
+    # Calcola la loss, l'errore percentuale medio e l'errore medio in mesi sui dati di test
     loss_fn = losses.MeanSquaredError()
     total_loss = 0.0
     total_percent_error = 0.0
+    total_absolute_error = 0.0  # Errore assoluto totale
     num_batches = 0
 
     for pooled_input, heatmap_input, gender_input, age_target in test_dataset:
         outputs = model(pooled_input, heatmap_input, gender_input, training=False)
         loss = loss_fn(age_target, tf.squeeze(outputs))
         total_loss += loss.numpy() * pooled_input.shape[0]
-        percent_errors = tf.abs((tf.squeeze(outputs) - age_target) / age_target) * 100.0
+
+        age_target_np = age_target.numpy()
+        outputs_np = tf.squeeze(outputs).numpy()
+        percent_errors = abs((age_target_np - outputs_np) / age_target_np) * 100
         total_percent_error += tf.reduce_mean(percent_errors).numpy() * pooled_input.shape[0]
+        total_absolute_error += tf.reduce_sum(abs(age_target_np - outputs_np))
         num_batches += pooled_input.shape[0]
 
-    avg_loss = total_loss / len(test_files)
-    avg_percent_error = total_percent_error / len(test_files)
+    avg_loss = total_loss / num_batches
+    avg_percent_error = total_percent_error / num_batches
+    avg_absolute_error = total_absolute_error / num_batches
+    avg_months_error = avg_absolute_error * 12  # Converte l'errore medio in mesi
 
-    print(f"Test Loss: {avg_loss:.4f}, Errore Percentuale Medio: {avg_percent_error:.2f}%")
-
+    print(f"Test Loss: {avg_loss:.4f}")
+    print(f"Test Errore Percentuale Medio: {avg_percent_error:.2f}%")
+    print(f"Test Errore Medio in Mesi: {avg_months_error:.2f} mesi")
--- src/training/train_dualbranchCNN.py
+++ src/training/train_dualbranchCNN.py
@@ -26,7 +26,7 @@ def process_folder(folder_path, tensors_dict):
 
 
 def train_model(model, train_files_path='data/Train/tensors', val_files_path='data/Val/tensors', num_epochs=20,
-                batch_size=32, learning_rate=1e-3, save_path="dual_branch_cnn_model.h5"):
+                batch_size=32, learning_rate=1e-3, save_path="out/dual_branch_cnn_model.h5"):
     """
     Funzione per addestrare il modello e salvare il modello addestrato (TensorFlow) con validation.
     """
@@ -47,8 +47,8 @@ def train_model(model, train_files_path='data/Train/tensors', val_files_path='da
         heatmap_tensor = load_tensor(file_prefix, "heated/.ATTRIBUTES/VARIABLE_VALUE")
 
         gender, age = filelabels_search(labels_path, id_img)
-        train_data["pooled"].append(pooled_tensor)
-        train_data["heatmap"].append(heatmap_tensor)
+        train_data["pooled"].append(tf.transpose(pooled_tensor, perm=[1, 2, 0]))  # added transpose
+        train_data["heatmap"].append(tf.transpose(heatmap_tensor, perm=[1, 2, 0]))  # added transpose
         train_data["gender"].append(gender)
         train_data["age"].append(age)
 
@@ -60,12 +60,12 @@ def train_model(model, train_files_path='data/Train/tensors', val_files_path='da
     train_age_tensors = tf.convert_to_tensor(train_data["age"], dtype=tf.float32)
     train_dataset = tf.data.Dataset.from_tensor_slices(
         (train_pooled_tensors, train_heatmap_tensors, train_gender_tensors, train_age_tensors))
-    train_dataset = train_dataset.shuffle(buffer_size=len(train_files)).batch(batch_size)
+    train_dataset = train_dataset.shuffle(buffer_size=len(train_files)).batch(batch_size, drop_remainder=True)
 
     # Carica i dati di validation
     val_files = [f for f in os.listdir(val_files_path) if f.endswith('.data-00000-of-00001')] #cambiato l'estensione
     val_data = {"pooled": [], "heatmap": [], "gender": [], "age": []}
-    val_labels_path = os.path.join(val_files_path, "..", "train_labels.csv")
+    val_labels_path = os.path.join(val_files_path, "..", "val_labels.csv")
     val_heatmaps_path = os.path.join(val_files_path, "..", "heatmaps")
     for file in val_files:
         file_prefix = os.path.join(val_files_path, file.split(".data")[0])
@@ -74,12 +74,12 @@ def train_model(model, train_files_path='data/Train/tensors', val_files_path='da
         except ValueError:
             print(f"Nome file non valido, saltato: {file}")
             continue
-        pooled_tensor = load_tensor(file_prefix.replace("heatmaps", "tensors"))
-        heatmap_tensor = load_tensor(file_prefix)
+        pooled_tensor = load_tensor(file_prefix.replace("heatmaps", "tensors"), "tensor/.ATTRIBUTES/VARIABLE_VALUE")
+        heatmap_tensor = load_tensor(file_prefix, "heated/.ATTRIBUTES/VARIABLE_VALUE")
 
         gender, age = filelabels_search(val_labels_path, id_img)
-        val_data["pooled"].append(pooled_tensor)
-        val_data["heatmap"].append(heatmap_tensor)
+        val_data["pooled"].append(tf.transpose(pooled_tensor, perm=[1, 2, 0]))  # added transpose
+        val_data["heatmap"].append(tf.transpose(heatmap_tensor, perm=[1, 2, 0]))  # added transpose
         val_data["gender"].append(gender)
         val_data["age"].append(age)
     val_data["gender"] = [gender_mapping[g] if isinstance(g, str) else g for g in val_data["gender"]]
@@ -89,40 +89,56 @@ def train_model(model, train_files_path='data/Train/tensors', val_files_path='da
     val_age_tensors = tf.convert_to_tensor(val_data["age"], dtype=tf.float32)
     val_dataset = tf.data.Dataset.from_tensor_slices(
         (val_pooled_tensors, val_heatmap_tensors, val_gender_tensors, val_age_tensors))
-    val_dataset = val_dataset.batch(batch_size)
+    val_dataset = val_dataset.batch(batch_size, drop_remainder=True)
 
     # Definizione dell'ottimizzatore e della funzione di loss
     optimizer = optimizers.Adam(learning_rate=learning_rate)
     loss_fn = losses.MeanSquaredError()
 
-    # Liste per salvare loss e errore percentuale medio di ogni epoch
     epoch_train_losses = []
     epoch_val_losses = []
-    epoch_train_percent_errors = []
-    epoch_val_percent_errors = []
 
-    # Abilita il plotting interattivo
     plt.ion()
     fig, ax = plt.subplots()
     ax.set_xlabel("Epoch")
-    ax.set_ylabel("Errore Percentuale Medio (%)")
-    ax.set_title("Andamento dell'Errore Percentuale Medio durante l'addestramento")
+    ax.set_ylabel("Loss")
+    ax.set_title("Training and Validation Loss")
 
     print("Inizio training...")
     for epoch in range(1, num_epochs + 1):
         # Training
         running_train_loss = 0.0
-        running_train_percent_error = 0.0
         for pooled_input, heatmap_input, gender_input, age_target in train_dataset:
             with tf.GradientTape() as tape:
                 outputs = model(pooled_input, heatmap_input, gender_input, training=True)
                 loss = loss_fn(age_target, tf.squeeze(outputs))
             gradients = tape.gradient(loss, model.trainable_variables)
             optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-            running_train_loss += loss.numpy() * pooled_input.shapeoled_input.shape[0]
+            running_train_loss += loss.numpy() * pooled_input.shape[0]
+        train_loss = running_train_loss / len(train_dataset)
+        epoch_train_losses.append(train_loss)
 
-    # Salva il modello alla fine del training
+        # Validation
+        running_val_loss = 0.0
+        for pooled_input, heatmap_input, gender_input, age_target in val_dataset:
+            outputs = model(pooled_input, heatmap_input, gender_input, training=False)
+            loss = loss_fn(age_target, tf.squeeze(outputs))
+            running_val_loss += loss.numpy() * pooled_input.shape[0]
+        val_loss = running_val_loss / len(val_dataset)
+        epoch_val_losses.append(val_loss)
+
+        # Aggiornamento plot
+        ax.clear()
+        ax.plot(epoch_train_losses, label='Training Loss')
+        ax.plot(epoch_val_losses, label='Validation Loss')
+        ax.legend()
+        plt.pause(0.1)
+
+        print(f"Epoch {epoch}/{num_epochs}: Training Loss = {train_loss:.4f}, Validation Loss = {val_loss:.4f}")
+
     model.save(save_path)
     print(f"Modello salvato in {save_path}")
+    plt.ioff()
+    plt.show()
 
-    return epoch_train_losses, epoch_val_losses, epoch_train_percent_errors, epoch_val_percent_errors
+    return epoch_train_losses, epoch_val_losses
